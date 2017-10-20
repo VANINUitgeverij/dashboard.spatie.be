1d3bd0446c437aca7548fbdcc73318f15f35deb0
Arne De Smedt <arne.desmedt@vanin.be>
2017-10-20 16:10:32 +0200

    feature - add timer to countdown

--- resources/assets/js/components/CountDown.vue
+++ resources/assets/js/components/CountDown.vue
@@ -1,29 +1,31 @@
 <template>
-    <tile v-if='!showOverlay' :position="position">
-        <section class="count-down">
-            <div class="count-down__content">
-                <div class="count-down__name">{{ name }}</div>
-                <div class="count-down__group">
-                    <div v-for="(value, key) in countDown" class="count-down__block">
-                        <div class="count-down__value">{{ value }}</div>
-                        <div class="count-down__name">{{ key }}{{ value == 1 ? '' : 's'}}</div>
+    <div>
+        <tile v-if='position' :position="position">
+            <section class="count-down">
+                <div class="count-down__content">
+                    <div class="count-down__name">{{ name }} {{ timer ? 'finished' : ''}} in</div>
+                    <div class="count-down__group">
+                        <div v-for="(value, key) in countDown" class="count-down__block">
+                            <div class="count-down__value">{{ value }}</div>
+                            <div class="count-down__name">{{ key }}{{ value == 1 ? '' : 's'}}</div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    </tile>
+            </section>
+        </tile>
 
-    <overlay v-else>
-        <div class="count-down__content">
-                <div class="count-down__name">{{ name }}</div>
-                <div class="count-down__group">
-                    <div v-for="(value, key) in countDown" class="count-down__block">
-                        <div class="count-down__value">{{ value }}</div>
-                        <div class="count-down__name">{{ key }}{{ value == 1 ? '' : 's'}}</div>
+        <overlay :show="showOverlay">
+            <div class="count-down__content">
+                    <div class="count-down__name">{{ name }} {{ timer ? 'finished' : ''}} in </div>
+                    <div class="count-down__group">
+                        <div v-for="(value, key) in countDown" class="count-down__block">
+                            <div class="count-down__value">{{ value }}</div>
+                            <div class="count-down__name">{{ key }}{{ value == 1 ? '' : 's'}}</div>
+                        </div>
                     </div>
                 </div>
-            </div>
-    </overlay>
+        </overlay>
+    </div>
 </template>
 
 <script>
@@ -46,14 +48,14 @@
                 type: String,
                 default: '55 10 * * 1-5',
             },
-            duration: {
-                type: Number,
-                default: 800, //15 minutes
-            },
             position: {
                 type: String,
             },
-            overlay: {
+            overlayMinutes: {
+                type: Number,
+                default: -1
+            },
+            timerMinutes: {
                 type: Number,
                 default: -1
             }
@@ -63,22 +65,49 @@
             return {
                 next: moment(),
                 time: moment(),
+                timer: false,
             };
         },
 
         computed: {
             countDown: function () {
-                var duration = moment.duration(this.next - this.time);
-                return {
+                var duration = this.getDuration();
+                var counter = {
                     day: duration.days(),
                     hour: duration.hours(),
                     minute: duration.minutes(),
                     second: duration.seconds(),
                 };
+
+                for (var key in counter) {
+                    if (counter[key] == 0) {
+                        delete counter[key];
+                        continue;
+                    }
+
+                    break;
+                }
+
+                if (Object.keys(counter).length == 0) {
+                    if(this.timerMinutes > 0) {
+                        if (this.timer) {
+                            this.init();
+                        } else {
+                            this.next = moment().add(this.timerMinutes, 'm');
+                        }
+                        this.timer = !this.timer;
+                    }
+                    
+                }
+
+                return counter;
             },
 
             showOverlay: function () {
-                if  (moment.duration(this.next - this.time).asMinutes() <= this.overlay) {
+                if  (
+                    this.getDuration().asMinutes() <= this.overlayMinutes ||
+                    this.timer
+                ) {
                     return true;
                 }
 
@@ -101,6 +130,10 @@
             refreshTime() {
                 this.time = moment();
             },
+
+            getDuration() {
+                return moment.duration(this.next - this.time)
+            }
         },
     };
 </script>
