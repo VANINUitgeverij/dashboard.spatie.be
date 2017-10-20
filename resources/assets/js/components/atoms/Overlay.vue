9460a578e345e5c000a7a297260923e823b339a1
Arne De Smedt <arne.desmedt@vanin.be>
2017-10-20 13:47:02 +0200

    feature - add overlay

---
+++ resources/assets/js/components/atoms/Overlay.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <transition 
+            appear 
+            name="blur-background"
+        >
+            <div v-if="show" class="overlay__background"></div>
+        </transition>
+        <transition 
+            appear 
+            name="fade-overlay"
+        >
+            <div v-if="show" class="overlay__container">
+                <slot></slot>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            show: {
+                type: Boolean,
+                default: false,
+            }
+        }
+    };
+</script>
