002432e70d2d38b04598247024e0dd7783d4d729
Bart Ducheyne <bart.ducheyne@vanin.be>
2018-04-20 16:06:19 +0200

    feature - make offline notification bigger (for .5 seconds)
    If you didn't noticed now you probably will have noticed on your own
    computer by that time or it will be fixed by that time.

--- resources/assets/js/components/InternetConnection.vue
+++ resources/assets/js/components/InternetConnection.vue
@@ -1,10 +1,12 @@
 <template>
-    <section v-if="offline" class="internet-connection">
-        <div class="internet-connection__alert">
-            <span class="internet-connection__icon h-icon"></span>
-            <span class="internet-connection__text">Internet connection</span>
-        </div>
-    </section>
+    <transition name="bounce">
+        <section v-if="offline" class="internet-connection">
+            <div class="internet-connection__alert">
+                <span class="internet-connection__icon h-icon"></span>
+                <span class="internet-connection__text">Internet connection</span>
+            </div>
+        </section>
+    </transition>
 </template>
 
 <script>
